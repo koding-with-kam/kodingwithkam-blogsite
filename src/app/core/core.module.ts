f6f07541fc8caf5bf6c7074bebace8eb340ccdcb
kameronhayes <kodingwithkam@gmail.com>
2022-10-04 01:12:11 -0400

    custom theme for material/setup layout

--- src/app/core/core.module.ts
+++ src/app/core/core.module.ts
@@ -3,13 +3,20 @@ import { CommonModule } from '@angular/common';
 import { FlexLayoutModule } from '@angular/flex-layout';
 import { LayoutModule } from '@angular/cdk/layout';
 import { RouterModule } from '@angular/router';
+import { MatSidenavModule } from '@angular/material/sidenav';
+import { MatToolbarModule } from "@angular/material/toolbar";
+import { MatButtonModule } from "@angular/material/button";
+import { MatMenuModule } from "@angular/material/menu";
+import { MatIconModule } from "@angular/material/icon";
 
 import { AppComponent } from './containers/app/app.component';
 import { NotFoundPageComponent } from './containers/not-found-page/not-found-page.component';
+import { HeaderComponent } from "./components/header/header.component";
 
 export const COMPONENTS = [
   AppComponent,
   NotFoundPageComponent,
+  HeaderComponent
 ];
 
 @NgModule({
@@ -19,7 +26,12 @@ export const COMPONENTS = [
     CommonModule,
     RouterModule,
     LayoutModule,
-    FlexLayoutModule
+    FlexLayoutModule,
+    MatSidenavModule,
+    MatToolbarModule,
+    MatButtonModule,
+    MatMenuModule,
+    MatIconModule
   ]
 })
 export class CoreModule { }
